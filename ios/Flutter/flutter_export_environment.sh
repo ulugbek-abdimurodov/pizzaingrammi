4f90c9f4c8334dba409a699d3f1c471d0c7afd21
Ulugbek Abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-07-08 17:40:42 +0200

    Deploy Flutter Web to gh-pages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/admiral/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/admiral/Documents/Work/Pizzaingrammi/pizzaingrammi-menu/pizzaingrammi_menu_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/admiral/Documents/Work/Pizzaingrammi/pizzaingrammi-menu/pizzaingrammi_menu_flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmNmMmMxMTU3Mg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZGQ5M2RlNmZiMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/admiral/Documents/Work/Pizzaingrammi/pizzaingrammi-menu/pizzaingrammi_menu_flutter/.dart_tool/package_config.json"
